5c4a481073669817985a0a0f5a4899597d325458
Glitch (glitch-blank-node) <none>
2023-05-14 22:51:19 +0000

    🌄🛣 Checkpoint ./middleware/multer-config.js:56515816/776 ./controllers/member.js:56515816/4

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,21 +1,21 @@
 const multer = require('multer')
 
+//dictionnaire
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+    'application/pdf': 'pdf'
+};
 
 const storage = multer.diskStorage({
     destination : (req, file, callback) => {
-        callback(null, './images/')
+        callback(null, './images')
     },
     filename: (req, file, callback) => {
         //file = req.files;
       
-        //dictionnaire
-        const MIME_TYPES = {
-            'image/jpg': 'jpg',
-            'image/jpeg': 'jpg',
-            'image/png': 'png',
-            'application/pdf': 'pdf'
-        };
-        const name = file.originalname.split('.')[0].split(' ').join('_');
+        const name = file.originalname.split('.')[0].split(' ').join('_').split('x').join('_');
         const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
